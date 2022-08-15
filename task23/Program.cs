ebd6b846665b9f5e19fb3cdbb3b34e78a95e2417
Ilya Fedorov <grenege13@gmail.com>
2022-08-15 23:33:37 +0300

    task23 qube set is done

---
+++ task23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N ");
+int numN = Convert.ToInt32(Console.ReadLine());
+int start = 1;
+
+Console.WriteLine("Список кубов до числа N : ");
+
+while (start <= numN)
+{
+    Console.WriteLine(Math.Pow(start, 3));
+    start++;
+}
